29da9bccf7a182f4fbb16c06cce2818a01f6cc2f
Vũ Đức Trung <vuductrung14112001@gmail.com>
2024-01-12 00:51:06 +0700

    Reload after crash

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,16 +2,47 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] private float delayTimeAfterCrash = 1f;
+    [SerializeField] private ParticleSystem crashVFX;
+
+    private bool _isCrashed = false;
+
+    private void Start()
+    {
+        this._isCrashed = false;
+    }
+
     private void OnCollisionEnter(Collision other)
     {
-        //Debug.Log($"{this.name} --Collide with-- {other.gameObject.name}");
+        Debug.Log($"{this.name} --Collide with-- {other.gameObject.name}");
+        StartCrashSequence();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        //Debug.Log($"{this.name} **Trigger with** {other.gameObject.name}");
+        Debug.Log($"{this.name} **Trigger with** {other.gameObject.name}");
+        StartCrashSequence();
+    }
+
+    void StartCrashSequence()
+    {
+        if (_isCrashed) return;
+
+        this._isCrashed = true;
+        crashVFX.Play();
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<PlayerControls>().enabled = false;
+        
+        Invoke(nameof(ReloadLevel), delayTimeAfterCrash);
+    }
+
+    void ReloadLevel()
+    {
+        int currSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currSceneIndex);
     }
 }
