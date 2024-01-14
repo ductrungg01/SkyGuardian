9772baac7f5440dc720b96291b51133943d83be5
Vũ Đức Trung <vuductrung14112001@gmail.com>
2024-01-14 14:09:22 +0700

    Score UI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,11 +7,30 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private GameObject deathVFX;
     [SerializeField] private Transform parentTransform;
-    
+    [SerializeField] private int scorePerHit = 15;
+
+    private ScoreBoard scoreBoard;
+
+    private void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
     private void OnParticleCollision(GameObject other)
     {
         //Debug.Log($"{this.name} I'm collide with {other.name}");
+        
+        ProcessHit();
+        KillEnemy();
+    }
 
+    void ProcessHit()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    void KillEnemy()
+    {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parentTransform;
         
